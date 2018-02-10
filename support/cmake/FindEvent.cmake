4ac68439d168c07460dfa372d6a95b69a8e06920
TomZ <tomz@freedommail.ch>
2018-02-10 14:59:45 +0100

    Introduce CMake building

---
+++ support/cmake/FindEvent.cmake
@@ -0,0 +1,15 @@
+# Find Libevent
+#
+# Once done, this will define:
+#
+#  Event_FOUND - system has Event
+#  Event_INCLUDE_DIRS - the Event include directories
+#  Event_LIBRARIES - link these to use Event
+#
+
+if (EVENT_INCLUDE_DIR AND EVENT_LIBRARY)
+  # Already in cache, be silent
+  set(EVENT_FIND_QUIETLY TRUE)
+endif (EVENT_INCLUDE_DIR AND EVENT_LIBRARY)
+
+pkg_check_modules(Event REQUIRED libevent_pthreads)
