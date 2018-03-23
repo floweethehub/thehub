7e99e9d5991840cf375b86f10a16a4df3301de3e
TomZ <tomz@freedommail.ch>
2018-03-23 01:14:52 +0100

    Update Qt names.

--- hub-qt/guiconstants.h
+++ hub-qt/guiconstants.h
@@ -59,10 +59,9 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_TESTNET_FT "Bitcoin-Qt-testnet-ft"
+#define QAPP_ORG_NAME "flowee"
+#define QAPP_ORG_DOMAIN "flowee.org"
+#define QAPP_APP_NAME_DEFAULT "FloweeTheHub"
+#define QAPP_APP_NAME_TESTNET "FloweeTheHub-testnet"
 
 #endif
