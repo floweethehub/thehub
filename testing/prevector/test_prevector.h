ce63d5165fa05b543e81502fd41e559f847039a4
TomZ <tomz@freedommail.ch>
2018-08-13 23:19:27 +0200

    Move prevector to its own test since its slow (16s)

---
+++ testing/prevector/test_prevector.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Flowee project
+ * Copyright (C) 2018 Tom Zander <tomz@freedommail.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef TEST_PREVECTOR_H
+#define TEST_PREVECTOR_H
+
+#include <QObject>
+#include <QtTest/QtTest>
+
+class TestPrevector : public QObject
+{
+    Q_OBJECT
+public:
+    TestPrevector() {}
+
+private slots:
+    void runTests();
+};
+
+#endif
