e2c735d6b81994f2dd9e74069d5fa1a67e39714d
TomZ <tomz@freedommail.ch>
2019-03-30 20:09:55 +0100

    Fix includes

--- libs/server/script/standard.h
+++ libs/server/script/standard.h
@@ -20,9 +20,8 @@
 #ifndef FLOWEE_SCRIPT_STANDARD_H
 #define FLOWEE_SCRIPT_STANDARD_H
 
-#include "script/interpreter.h"
-#include "uint256.h"
-
+#include "interpreter.h"
+#include <uint256.h>
 #include <boost/variant.hpp>
 
 class CKeyID;
