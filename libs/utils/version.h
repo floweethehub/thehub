26747573f737e52562fffb372d4fd27cd4c590e6
TomZ <tomz@freedommail.ch>
2019-05-08 10:53:18 +0200

    Import protocol version numbers

--- libs/utils/version.h
+++ libs/utils/version.h
@@ -23,16 +23,13 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80002;
+static const int PROTOCOL_VERSION = 70012;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 31800;
-
-//! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -51,7 +48,21 @@ static const int MEMPOOL_GD_VERSION = 60002;
 //! "sendheaders" command and announcing blocks with headers starts with this version
 static const int SENDHEADERS_VERSION = 70012;
 
+//! "feefilter" tells peers to filter invs to you by fee starts with this
+//! version
+static const int FEEFILTER_VERSION = 70013;
+
+//! short-id-based block download starts with this version
+static const int SHORT_IDS_BLOCKS_VERSION = 70014;
+
+//! not banning for invalid compact blocks starts with this version
+static const int INVALID_CB_NO_BAN_VERSION = 70015;
+
 //! Expedited Relay enabled in this version
 static const int EXPEDITED_VERSION = 80002;
+
+
+//! disconnect from peers older than this proto version
+static const int MIN_PEER_PROTO_VERSION = SENDHEADERS_VERSION;
 
 #endif
