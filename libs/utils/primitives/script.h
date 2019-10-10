d684bb5b5214f2858e0c8a6a88b8ca8420cdf1b4
TomZ <tomz@freedommail.ch>
2019-10-10 18:11:34 +0200

    Remove dead code.

--- libs/utils/primitives/script.h
+++ libs/utils/primitives/script.h
@@ -564,14 +564,6 @@ public:
         while (GetOp(pc, opcode));
         return nFound;
     }
-    int Find(opcodetype op) const {
-        int nFound = 0;
-        opcodetype opcode;
-        for (const_iterator pc = begin(); pc != end() && GetOp(pc, opcode);)
-            if (opcode == op)
-                ++nFound;
-        return nFound;
-    }
 
     /**
      * Pre-version-0.6, Bitcoin always counted CHECKMULTISIGs
