3b562b00e5d67500bd7ef31dd7c95fb00cebe29e
TomZ <tomz@freedommail.ch>
2020-09-09 11:46:06 +0200

    Include for the namespace

---
+++ libs/utils/Streaming.h
@@ -0,0 +1,6 @@
+#include "streaming/BufferPool.h"
+#include "streaming/ConstBuffer.h"
+#include "streaming/MessageBuilder.h"
+#include "streaming/MessageParser.h"
+#include "streaming/P2PBuilder.h"
+#include "streaming/P2PParser.h"
