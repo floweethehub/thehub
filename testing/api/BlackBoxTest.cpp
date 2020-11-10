e58a10fb6d2bc96bc9b98b80c482efeed5fa36e4
TomZ <tomz@freedommail.ch>
2020-11-10 18:48:03 +0100

    Forgot the copyright license...

--- testing/api/BlackBoxTest.cpp
+++ testing/api/BlackBoxTest.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Flowee project
+ * Copyright (C) 2019-2020 Tom Zander <tomz@freedommail.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "BlackBoxTest.h"
 
 #include <QTime>
@@ -46,7 +63,7 @@ void BlackBoxTest::startHubs(int amount, Connect connect)
     m_currentTest = QString(QTest::currentTestFunction());
     m_baseDir = QDir::tempPath() + QString("/flowee-bbtest-%1").arg(rand());
     logDebug() << "Starting hub at" << m_baseDir << "with" << s_hubPath;
-    int port = rand() % 31000 + 1000;
+    int port = rand() % 31000 + 1025;
     for (int i = 0; i < amount; ++i) {
         Hub hub;
         hub.proc = new QProcess(this);
