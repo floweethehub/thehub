abfc90d99a4c2e1d30f145b0e1e34af8877c818f
TomZ <tom@flowee.org>
2021-02-17 22:59:35 +0100

    assert -> if

--- libs/networkmanager/NetworkManager_p.h
+++ libs/networkmanager/NetworkManager_p.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Flowee project
- * Copyright (C) 2016, 2019-2020 Tom Zander <tomz@freedommail.ch>
+ * Copyright (C) 2016-2021 Tom Zander <tom@flowee.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -217,10 +217,10 @@ public:
 
     inline void disconnect() {
         close(false);
-        assert(m_priorityMessageQueue);
-        m_priorityMessageQueue->clear();
-        assert(m_messageQueue);
-        m_messageQueue->clear();
+        if (m_priorityMessageQueue)
+            m_priorityMessageQueue->clear();
+        if (m_messageQueue)
+            m_messageQueue->clear();
     }
 
     void recycleConnection();
